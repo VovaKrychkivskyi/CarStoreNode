f2921a92c4eaa6da6f893df7a72fc241e161ce8b
VovaKrychkivskyi <4Fa263izmnSurDnTa8fY>
2020-09-30 14:20:17 +0300

    router

--- src/controllers/user.controllers/create.user.controller.js
+++ src/controllers/user.controllers/create.user.controller.js
@@ -8,7 +8,6 @@ const {
 
 module.exports = {
   createUser: async (req, res) => {
-    console.log(22);
     const user = await createUserService(req.body)
     res.json(user)
 
--- src/services/user.servise/createUser.js
+++ src/services/user.servise/createUser.js
@@ -2,7 +2,6 @@ const UserModel = require(`../../database/models/user.model`)
 module.exports = {
 
   createUserService: async (user) => {
-    console.log(35);
     return await UserModel.create(user)
   }
 }
