361e407b28e6183be1e3e8f2d13c0b13c7aafaa4
VovaKrychkivskyi <4Fa263izmnSurDnTa8fY>
2020-09-30 10:28:07 +0300

    migrate

---
+++ src/configs/database.config.js
@@ -0,0 +1,9 @@
+module.exports={
+  "development": {
+    "username": "root",
+    "password": "qw1234qw",
+    "database": "car_store",
+    "host": "127.0.0.1",
+    "dialect": "mysql"
+  }
+}
---
+++ src/database/migrations/20200930065936-user.js
@@ -0,0 +1,44 @@
+'use strict';
+
+const {DataTypes} = require(`sequelize`)
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    /**
+     * Add altering commands here.
+     *
+     * Example:
+     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
+     */
+
+    await queryInterface.createTable(`users_table`, {
+      id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+      },
+      name: {
+        type: DataTypes.STRING,
+        allowNull: false
+      },
+      password: {
+        type: DataTypes.STRING,
+        allowNull: false
+      },
+      email: {
+        type: DataTypes.STRING,
+        allowNull: false
+      }
+    })
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    /**
+     * Add reverting commands here.
+     *
+     * Example:
+     * await queryInterface.dropTable('users');
+     */
+    await queryInterface.dropTable(`users_table`)
+  }
+}
