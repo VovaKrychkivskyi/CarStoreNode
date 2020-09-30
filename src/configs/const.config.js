aad05a128477dca033ae4f5419db9f6ac7226b98
VovaKrychkivskyi <4Fa263izmnSurDnTa8fY>
2020-09-30 17:34:14 +0300

    errors

--- src/configs/const.config.js
+++ src/configs/const.config.js
@@ -6,5 +6,15 @@ module.exports={
   HOST:process.env.HOST || `127.0.0.1`,
   DIALECT:process.env.DIALECT || `mysql`,
 
-  TABLE_NAME_USER:process.env.TABLE_NAME_USER
+  AUTHORIZATION: process.env.AUTHORIZATION,
+
+  TABLE_NAME_OAUTH: process.env.TABLE_NAME_OAUTH,
+  TABLE_NAME_USER: process.env.TABLE_NAME_USER,
+  TABLE_NAME_CAR: process.env.TABLE_NAME_CAR,
+
+  MAX_PHOTO_SIZE: process.env.MAX_PHOTO_SIZE,
+  MAX_DOC_SIZE: process.env.MAX_DOC_SIZE,
+
+  PHOTO_MIMETYPES:process.env.PHOTO_MIMETYPES,
+  DOCS_MIMETYPES:process.env.DOCS_MIMETYPES,
 }
